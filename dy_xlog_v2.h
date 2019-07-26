79360d62077a65e88bf3c304dc10ee5443e52754
Fb <qazx84265@163.com>
2019-07-26 23:50:48 +0800

    Add files via upload

---
+++ dy_xlog_v2.h
@@ -0,0 +1,13 @@
+
+#ifndef dy_xlog_v2_h
+#define dy_xlog_v2_h
+
+#include <stdint.h>
+
+//en
+void x_en(const char* as, uint32_t asLen, char*& xl, uint32_t* xlLen);
+
+//de
+void sub_1019EF3B4(const char *en, uint32_t el, char*& de, uint32_t* deLen);
+
+#endif
---
+++ urlParser.h
@@ -0,0 +1,70 @@
+﻿//
+//  urlParser.h
+//  DyMasServer
+//
+//  Created by  on 2018/6/29.
+//  Copyright © 2018年 . All rights reserved.
+//
+
+#ifndef urlParser_h
+#define urlParser_h
+
+#include <string>
+using namespace std;
+
+
+class UrlParser
+{
+public:
+    UrlParser(const char* url);
+    const string GetScheme() const
+    {
+        return m_scheme;
+    }
+    const string GetHost() const
+    {
+        return m_host;
+    }
+    const string GetPort() const
+    {
+        return m_port;
+    }
+    const string GetPath() const
+    {
+        return m_path;
+    }
+    const string GetQuery() const
+    {
+        return m_query;
+    }
+    const string GetFragment() const
+    {
+        return m_fragment;
+    }
+    const string GetUserName() const
+    {
+        return m_username;
+    }
+    const string GetPassword() const
+    {
+        return m_password;
+    }
+private:
+    bool Init(const char* url);
+    bool IsSchemeChar(int c)
+    {
+        return (!isalpha(c) && '+' != c && '-' != c && '.' != c) ? 0 : 1;
+    }
+private:
+    string m_scheme;               /* mandatory */
+    string m_host;                 /* mandatory */
+    string m_port;                 /* optional */
+    string m_path;                 /* optional */
+    string m_query;                /* optional */
+    string m_fragment;             /* optional */
+    string m_username;             /* optional */
+    string m_password;             /* optional */
+};
+
+
+#endif /* urlParser_h */
