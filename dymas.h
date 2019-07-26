11823f042009853e3f3befe72bf076dc2406fda7
Fb <qazx84265@163.com>
2019-07-26 23:53:34 +0800

    Update and rename dy_mas_v1.h to dymas.h

--- dymas.h
+++ dymas.h
@@ -6,8 +6,8 @@
 //  Copyright © 2018年 . All rights reserved.
 //
 
-#ifndef dy_mas_v1_h
-#define dy_mas_v1_h
+#ifndef dymas_h
+#define dymas_h
 
 //mas
 void cb4da8435e82e69c0271ba133bb9cd8d(const char* as, int asLen, int a3, char* &mas, int &masLen);
@@ -15,4 +15,4 @@ void cb4da8435e82e69c0271ba133bb9cd8d(const char* as, int asLen, int a3, char* &
 //xlog encode
 void b71bb87dba5d115f9770ab296ae81201(const char* src, int srcLen, char* &pout, int &outLen);
 
-#endif /* dy_mas_v1_h */
+#endif /* dymas_h */
