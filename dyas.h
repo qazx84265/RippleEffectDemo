422e018a88051db3447e3f7126d929e9033930c4
Fb <qazx84265@163.com>
2019-07-26 23:38:23 +0800

    Create dyas.h

---
+++ dyas.h
@@ -0,0 +1,20 @@
+//
+//  dy_as_v1.h
+//  
+//
+//  Created by  on 2018/3/23.
+//
+
+#ifndef dy_as_v1_h
+#define dy_as_v1_h
+
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+void splitString(const string& s, vector<string>& v, const string& c);
+
+string fc74654a61f3727e6adfce1cff5025f(char* urlSt, unsigned int& ti);
+
+#endif /* dy_as_v1_h */
